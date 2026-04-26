c80251a8d96d1444611c6ad1f1df8fc124deba62
reda <rm9396800@gmail.com>
2026-04-27 01:12:44 +0300

    added post delete and put, and added data to data.json as well

--- server.js
+++ server.js
@@ -6,53 +6,122 @@ const dataPath = path.join(__dirname, 'data.json');
 
 const server = http.createServer((req, res) => {
 
-    
+    const readData = () => {
+        return fs.readFileSync(dataPath, 'utf-8');
+    };
+
+    const writeData = (data) => {
+        fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+    };
+
     if (req.url === '/api/movies' && req.method === 'GET') {
+        const movies = readData();
 
-        fs.readFile(dataPath, 'utf-8', (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                return res.end(JSON.stringify({ message: "Error reading file" }));
-            }
-
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(data);
-        });
-
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        return res.end(movies);
     }
 
-   
     else if (req.url.startsWith('/api/movies/') && req.method === 'GET') {
-
         const id = parseInt(req.url.split('/')[3]);
 
-        fs.readFile(dataPath, 'utf-8', (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                return res.end(JSON.stringify({ message: "Error reading file" }));
-            }
+        const movies = JSON.parse(readData());
+        const movie = movies.find(m => m.id === id);
 
-            const movies = JSON.parse(data);
-            const movie = movies.find(m => m.id === id);
+        if (!movie) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            return res.end(JSON.stringify({ message: "Movie not found" }));
+        }
 
-            if (!movie) {
-                res.writeHead(404, { 'Content-Type': 'application/json' });
-                return res.end(JSON.stringify({ message: "Movie not found" }));
-            }
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        return res.end(JSON.stringify(movie));
+    }
 
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(movie));
+    else if (req.url === '/api/movies' && req.method === 'POST') {
+        let body = '';
+        req.setEncoding('utf-8');
+
+        req.on('data', chunk => {
+            body += chunk;
         });
 
+        req.on('end', () => {
+            try {
+                const newMovie = JSON.parse(body);
+                const movies = JSON.parse(readData());
+
+                newMovie.id = movies.length ? movies[movies.length - 1].id + 1 : 1;
+
+                movies.push(newMovie);
+                writeData(movies);
+
+                res.writeHead(201, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(newMovie));
+            } catch (err) {
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ message: "Invalid JSON format" }));
+            }
+        });
+    }
+
+    else if (req.url.startsWith('/api/movies/') && req.method === 'PUT') {
+        const id = parseInt(req.url.split('/')[3]);
+
+        let body = '';
+        req.setEncoding('utf-8');
+
+        req.on('data', chunk => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            try {
+                const updatedMovie = JSON.parse(body);
+                const movies = JSON.parse(readData());
+
+                const index = movies.findIndex(m => m.id === id);
+
+                if (index === -1) {
+                    res.writeHead(404, { 'Content-Type': 'application/json' });
+                    return res.end(JSON.stringify({ message: "Movie not found" }));
+                }
+
+                movies[index] = { id, ...updatedMovie };
+                writeData(movies);
+
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(movies[index]));
+            } catch (err) {
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ message: "Invalid JSON format" }));
+            }
+        });
+    }
+
+    else if (req.url.startsWith('/api/movies/') && req.method === 'DELETE') {
+        const id = parseInt(req.url.split('/')[3]);
+
+        const movies = JSON.parse(readData());
+        const filtered = movies.filter(m => m.id !== id);
+
+        if (movies.length === filtered.length) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            return res.end(JSON.stringify({ message: "Movie not found" }));
+        }
+
+        writeData(filtered);
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: "Movie deleted successfully" }));
     }
 
     else {
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: "Server is online and ready for requests" }));
+        res.end(JSON.stringify({ message: "Movie API running" }));
     }
 });
 
 const PORT = 3000;
+
 server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
